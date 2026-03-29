cc0dfd734bf708fc11b949b0bb5c9e98c7e2585c
Vikas Gavandi <30316401+vikasgavandi@users.noreply.github.com>
2026-03-29 12:51:32 +0530

    fix: use PM2 fork mode to prevent EADDRINUSE port conflict

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'vetforumindia-backend',
       cwd: './vetforum-backend',
       script: 'server.js',
+      exec_mode: 'fork',
       instances: 1,
       autorestart: true,
       watch: false,
